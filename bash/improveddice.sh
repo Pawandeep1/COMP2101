bdace765a83c20e12fddd801b50e65ecb4b7b9d7
Pawandeep1 <200479632@student.georgianc.on.ca>
2022-02-01 01:29:04 +0000

    Created improveddice.sh

---
+++ bash/improveddice.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# this script rolls a pair of six-sided dice and displays both the rolls
+#
+
+# Task 1:
+#  put the number of sides in a variable which is used as the range for the random number
+#  put the bias, or minimum value for the generated number in another variable
+#  roll the dice using the variables for the range and bias i.e. RANDOM % range + bias
+
+# Task 2:
+#  generate the sum of the dice
+#  generate the average of the dice
+
+#  display a summary of what was rolled, and what the results of your arithmetic were
+
+er we have started processingix-sided die, opposite faces are arranged to always sum to seven. This gives two possible mirror image arrangements in which the numbers 1, 2, and 3 may be arranged in a clockwise or counterclockwise order about a corner.                                                   Average of dice is 3.5
+#Tell the user we have started processing
+echo "Rolling..."
+# roll the dice and save the results
+die1=$(( RANDOM % 6 + 1))
+die2=$(( RANDOM % 6 + 1 ))
+# display the results
+echo "Rolled $die1, $die2"
